d89074844dc07ff0d6f1c2a11d8052c10f9d3545
250311814 <250311814@qq.com>
2021-04-09 11:00:01 +0800

    version 1.0.2

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,5 +2,7 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git上传的项目");
         System.out.println("隐约雷鸣,阴霾天空,但盼风雨来,能留你在此.");
+        System.out.println("你写的啥?爬!");
+        System.out.println("尊贵1.0.2");
     }
 }
